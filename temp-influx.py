2e677f7c143bfe965890645935922c7751d8ef66
Maurice Daverveldt <maurice.daverveldt@gmail.com>
2020-12-23 10:48:44 +0100

    Added support for config

--- temp-influx.py
+++ temp-influx.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python
 
+import json
+from argparse import ArgumentParser
 import time
 import datetime
 import random
 from influxdb import InfluxDBClient
 
-influx_host = 'raspberrypi-unifi.local'
-host = '127.0.0.1'
-port = 8086
-dbname = 'home'
-user = 'grafana'
-password = 'hidden'
+parser = ArgumentParser("Read ds18b20 sensor and post to influxDb")
+parser.add_argument("--config", required=True)
 
 temp_dir = "/sys/bus/w1/devices/w1_bus_master1/"
-temp_1_name = "28-00000bc1308b"
-temp_2_name = "28-00000bc92225"
-
-client = InfluxDBClient(influx_host, port, user, password, dbname)
 
 def temp_raw(name):
     f = open(temp_dir + name + "/w1_slave", 'r')
@@ -35,14 +29,14 @@ def read_temp(name):
         temp_c = float(temp_string) / 1000.0
         return temp_c
 
-def get_data_points():
+def get_data_points(sensors):
     iso = time.ctime()
-    temp_1 = read_temp(temp_1_name)
-    temp_2 = read_temp(temp_2_name)
+    temp_1 = read_temp(sensors[0]["id"])
+    temp_2 = read_temp(sensors[1]["id"])
     json_body = [
             {
                 "measurement": "temperature",
-                "tags": {"host": host},
+                "tags": {"host": "127.0.0.1"},
                 "time": datetime.datetime.utcnow(),
                 "fields": {
                     "temp_1": temp_1,
@@ -54,9 +48,22 @@ def get_data_points():
 
     return json_body
 
+config_file = parser.parse_args().config
 
-while True:
-    json_body = get_data_points()
-    client.write_points(json_body)
-    print (json_body)
-    time.sleep(1)
+with open(config_file) as f:
+    config = json.load(f)
+
+if __name__ == "__main__":
+    client = InfluxDBClient(
+        config["influx_config"]["influx_host"],
+        config["influx_config"]["port"],
+        config["influx_config"]["user"],
+        config["influx_config"]["password"],
+        config["influx_config"]["dbname"])
+
+    while True:
+        json_body = get_data_points(config["location"][1]["sensors"])
+        client.write_points(json_body)
+        print (json_body)
+        time.sleep(1)
+
